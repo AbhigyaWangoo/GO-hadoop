2a5d0afd6402aa160e28b611e84f1d85e67576d3
awangoo2 <awangoo2@fa23-cs425-4902.cs.illinois.edu>
2023-09-24 11:01:55 -0500

    Making suspicion default for testing purposes

--- server/gossip/gossipUtils/utils.go
+++ server/gossip/gossipUtils/utils.go
@@ -44,7 +44,7 @@ var MembershipMap cmap.ConcurrentMap[string, Member]
 var MembershipUpdateTimes cmap.ConcurrentMap[string, int64]
 var Ip string
 var MessageDropRate float32 = 0.0
-var ENABLE_SUSPICION bool = false
+var ENABLE_SUSPICION bool = true
 var LogFile = GetLogFilePointer()
 
 var GossipMutex sync.Mutex
