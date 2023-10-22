c3c23f56a91aa95a7a8531f8dc5a6635eabc264a
awangoo2 <awangoo2@fa23-cs425-4901.cs.illinois.edu>
2023-10-22 14:21:13 -0500

    Further restructuring and skeleton pseudocode writing

--- server/sdfs/sdfsUtils/utils.go
+++ server/sdfs/sdfsUtils/utils.go
@@ -1,16 +1,99 @@
 package sdfsutils
 
-type Command int
-
-const (
-	READ         Command = 0
-	WRITE         Command = 1
-	DELETE      Command = 2
+import (
+	"fmt"
+	"net"
+	"os"
 )
 
+type BlockOperation int
+
+// Potentially use send, receive, write, delete types instead types instead
+const (
+	READ   BlockOperation = 0
+	WRITE  BlockOperation = 1
+	DELETE BlockOperation = 2
+	GET_2D BlockOperation = 3
+)
+
+type Task struct {
+	DataTargetIp        string
+	AckTargetIp         string
+	ConnectionOperation BlockOperation // READ, WRITE, GET_2D, OR DELETE from sdfs utils
+	FileName            string
+	BlockIndex          int
+	DataSize            int
+	IsAck               bool
+}
+
 const KB int = 1024
+const MB int = KB * 1024
 const SDFS_PORT string = "3541"
-const SDFS_BUFFER_SIZE int = 4 * KB
+const SDFS_ACK_PORT string = "9682"
 const FILESYSTEM_ROOT string = "sdfs/sdfsFileSystemRoot/"
+const BLOCK_SIZE int = 128 * MB
+const REPLICATION_FACTOR int = 4
 
 var MASTER_IP string = "172.22.158.162"
+
+// Opens a tcp connection to the provided ip address and port, and returns the connection object
+func OpenTCPConnection(IpAddr string, Port string) (net.Conn, error) {
+	// Concatenate IP address and port to form the address string
+	address := IpAddr + ":" + Port
+
+	// Attempt to establish a TCP connection
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		// Handle error if connection fails
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+
+	// Connection successful, return connection object and nil error
+	return conn, nil
+}
+
+func ListenOnTCPConnection(Port string) (net.Listener, error) {
+	serverAddr, resolveErr := net.ResolveTCPAddr("tcp", Port)
+	if resolveErr != nil {
+		fmt.Println("Error resolving address:", resolveErr)
+		os.Exit(1)
+	}
+
+	tcpConn, listenErr := net.ListenTCP("tcp", serverAddr)
+	if listenErr != nil {
+		fmt.Println("Error listening:", listenErr)
+		os.Exit(1)
+	}
+
+	return tcpConn, nil
+}
+
+func GetFileSize(filePath string) int64 {
+	// Get file information
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return -1
+	}
+
+	// Get file size in bytes
+	fileSize := fileInfo.Size()
+
+	// Print the file size
+	return fileSize
+}
+
+func CeilDivide(a, b int64) int64 {
+	// Perform integer division
+	quotient := a / b
+
+	// Check if there is a remainder
+	remainder := a % b
+	if remainder > 0 {
+		// If there is a remainder, round up the quotient
+		quotient++
+	}
+
+	return quotient
+}
--- server/sdfs/sdfs_client.go
+++ server/sdfs/sdfs_client.go
@@ -1,25 +1,62 @@
 package sdfs
 
-func SdfsCLI() {
+import "fmt"
+
+func RequestBlockMappings(FileName string) [][]string {
+	// 1. Create a task with the GET_2D block operation, and send to current master. If timeout/ doesn't work, send to 1st submaster, second, and so on.
+	// 2. Listen for 2d array on responding connection. Read 2d array and return it.
+
+	var rv [][]string
+
+	return rv
+}
+
+func InitiatePutCommand(LocalFilename string, SdfsFilename string) {
+	fmt.Printf("localfilename: %s sdfs: %s\n", LocalFilename, SdfsFilename)
+
+	// IF CONNECTION CLOSES WHILE WRITING, WE NEED TO REPICK AN IP ADDR. Can have a seperate function to handle this on failure cases.
+
+	// 1. Create 2d array of ip addressses
+	// 		Call InitializeBlockLocationsEntry(), which should init an empty array for a filename.
+	// 2. For i = 0; i < num_blocks; i ++
+	// 		2.a. Construct a List of FollowerTasks, with the ack target as the master and DataTargetIp as empty
+	// 		2.b. Open Connections to all ips in 2darr[i], and write tasks to all ips.
+	// 		2.c. buffered read localfilename[i:i+block_size] (4kb at a time should work, check utils for KB variable)
+	// 		2.d. IN BUFFERED READ FUNCTION -> for ip in 2darr[i]:
+	// 				2.d.a. Spawn a thread to write current read block to ip with connections previously opened
+	//
 
 }
 
-func InitiatePutCommand() {
+func InitiateGetCommand(sdfs_filename string, localfilename string) {
+	fmt.Printf("localfilename: %s sdfs: %s\n", localfilename, sdfs_filename)
+
+	// IF CONNECTION CLOSES WHILE READING, WE NEED TO REPICK AN IP ADDR TO READ FROM. Can have a seperate function to handle this on failure cases.
+
+	// 1. Query master for 2d array of ip addresses (2darr)
+	// 2. For i = 0; i < num_blocks; i ++
+	// 		### at the beginning of every loop, need to fseek(i*block_size) ###
+	// 		2.a. Randomly pick IP addresses from 2darr[i] until you can open a connection to one successfully. If impossible, re-request 2d array.
+	// 		2.b. Construct a FollowerTask with the operation=READ, blockidx=i, filename=sdfs_filename, acktarget=master, datatarget="" (IMPORTANT, IF DATATARGET IS EMPTY, IT MEANS JUST SEND DATA BACK ON THE SAME CONNECTION)
+	// 		2.c. buffered read from connection (4kb at a time should work, check utils for KB variable)
+	// 		2.d. IN BUFFERED READ FUNCTION -> write buffered array to localfilename.
+}
+
+func InitiateDeleteCommand(sdfs_filename string) {
+	fmt.Printf("sdfs: %s\n", sdfs_filename)
+	// IF CONNECTION CLOSES WHILE READING, its all good. We can assume memory was wiped
+
+	// 1. Query master for 2d array of ip addresses (2darr)
+	// 2. For i = 0; i < num_blocks; i ++
+	// 3. 		For j = 0; j < num_replicas; j++
+	// 				3.a. Create a delete task struct, with master as ack target, and send to ip addr.
 
 }
 
-func InitiateGetCommand() {
-	
-}
-
-func InitiateDeleteCommand() {
-	
-}
-
-func InitiateLsCommand() {
+func InitiateLsCommand(sdfs_filename string) {
 
 }
 
 func InitiateStoreCommand() {
-	
+
 }
