bae2bb528c8a248bbc876f3fe0ccb55e36b3eff0
awangoo2 <awangoo2@fa23-cs425-4901.cs.illinois.edu>
2023-11-02 17:58:57 -0500

    Hrd set buff size

--- server/sdfs/sdfsUtils/utils.go
+++ server/sdfs/sdfsUtils/utils.go
@@ -147,7 +147,7 @@ func BufferedReadAndWrite(conn *bufio.ReadWriter, fp *os.File, size uint32, from
 	if fromLocal {
 		w = conn.Writer
 		r = bufio.NewReader(fp)
-		bufferSize = size * 3 / 4
+		bufferSize = uint32(MB * 3 / 4)
 	} else {
 		w = bufio.NewWriter(fp)
 		r = conn.Reader
