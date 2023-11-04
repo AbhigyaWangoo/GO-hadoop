0b4311365911eba3fbdd047cdbc00c9402bb1f4f
asehgal4 <asehgal4@fa23-cs425-4901.cs.illinois.edu>
2023-11-04 17:09:38 -0500

    Stuff

--- server/sdfs/sdfsUtils/utils.go
+++ server/sdfs/sdfsUtils/utils.go
@@ -43,7 +43,7 @@ type Task struct {
 
 const KB = int64(1024)
 const MB = int64(KB * 1024)
-const SDFS_PORT = "3456"
+const SDFS_PORT = "3888"
 const SDFS_ACK_PORT = "9682"
 const FILESYSTEM_ROOT = "server/sdfs/sdfsFileSystemRoot/"
 const BLOCK_SIZE = int64(5 * MB)
