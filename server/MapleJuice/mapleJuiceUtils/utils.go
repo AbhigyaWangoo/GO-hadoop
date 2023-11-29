3ac750660c2d0e1461e7693647e92e4b5eb3ed37
asehgal4 <asehgal4@fa23-cs425-4901.cs.illinois.edu>
2023-11-29 17:21:36 -0600

    Changed ports

--- server/MapleJuice/mapleJuiceUtils/utils.go
+++ server/MapleJuice/mapleJuiceUtils/utils.go
@@ -19,7 +19,7 @@ const RANGE PartitioningType = 1
 const MAPLE MapleJuiceType = 0
 const JUICE MapleJuiceType = 1
 
-const MAPLE_JUICE_PORT = "7898"
+const MAPLE_JUICE_PORT = "6000"
 
 type MapleJuiceTask struct {
 	Type            MapleJuiceType
